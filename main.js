894eb6ceb1157c76c7ec73191be3b9a68f2ca4ee
Vinith Didgi <138802852+Vinith0606@users.noreply.github.com>
2024-09-30 18:54:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Python Developer", "AI Engineer", "Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
