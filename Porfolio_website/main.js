74d74a404bef79d19d0fd9776b6fd0449a6a8899
Vinith Didgi <138802852+Vinith0606@users.noreply.github.com>
2024-09-30 18:52:32 +0530

    Delete Porfolio_website directory

--- Porfolio_website/main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text",{
-    strings:["Python Developer", "AI Engineer", "Web Developer"],
-    typeSpeed : 10,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
-});
